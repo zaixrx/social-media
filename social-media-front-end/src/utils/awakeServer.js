eba4073d84df20a7018e49a9bd3a76fd8ebc80a5
zaixrx <zaizroff@gmail.com>
2024-09-02 17:49:08 +0100

    Fixed it

--- social-media-front-end/src/utils/awakeServer.js
+++ social-media-front-end/src/utils/awakeServer.js
@@ -1,10 +1,12 @@
 import axios from "axios";
-import cron from "cron";
 
-export default new cron.CronJob("*/14 * * * *", () => {
-  axios
-    .get(process.env.REACT_APP_API_URL)
-    .catch(({ message, response }) =>
-      console.log(response ? response : message)
-    );
-});
+export default {
+  start: () =>
+    setInterval(() => {
+      axios
+        .get(process.env.REACT_APP_API_URL)
+        .catch(({ message, response }) =>
+          console.error(response ? response : message)
+        );
+    }, 1000 * 60 * 14),
+};
