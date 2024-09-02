371215c216f148d51ce2e20b9c44af59b0026749
zaixrx <zaizroff@gmail.com>
2024-09-02 17:44:11 +0100

    Update awakeServer.js

--- social-media-front-end/src/utils/awakeServer.js
+++ social-media-front-end/src/utils/awakeServer.js
@@ -2,9 +2,8 @@ import axios from "axios";
 import cron from "cron";
 
 export default new cron.CronJob("*/14 * * * *", () => {
-  console.log("Waking up the server");
   axios
-    .get(apiURL)
+    .get(process.env.REACT_APP_API_URL)
     .catch(({ message, response }) =>
       console.log(response ? response : message)
     );
