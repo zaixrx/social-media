0110e740d8466990e7a8fa6899cc1685ec83d0f0
Mohamed Anis KOUA <102376766+zaixrx@users.noreply.github.com>
2025-06-03 08:56:44 +0100

    Update wakeUpServer.js

--- social-media-back-end/wakeUpServer.js
+++ social-media-back-end/wakeUpServer.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 // Used to not let the server shut down after 15 minutes.
 module.exports = function () {
-  setInterval(() => {
+  /*setInterval(() => {
     axios
       .get(process.env.REFRESH_API_ENDPOINT)
       .then(() => console.log("Server Wake Up"))
       .catch(({ message, response }) =>
         console.error(response ? response : message)
       );
-  }, 1000 * 60 * 15);
+  }, 1000 * 60 * 15);*/
 };
